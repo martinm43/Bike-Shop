97a4ee8e3a2f1497a68868ce741d293d239425f4
MA Miller <miller.m.43@gmail.com>
2020-08-01 16:01:50 -0400

    Add project files.

---
+++ Bike Shop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Bike_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Integrated Security=SSPI is required for Windows Authentication login.
+            var cs = @"Server=localhost\SQLEXPRESS;Database=bikeshop;Trusted_Connection=True;Integrated Security=SSPI;";
+            var stm = "SELECT @@VERSION";
+
+            using var con = new SqlConnection(cs);
+            con.Open();
+
+            using var cmd = new SqlCommand(stm, con);
+            string version = cmd.ExecuteScalar().ToString();
+
+            Console.WriteLine(version);
+        }
+    }
+}
---
+++ customer_list.sql
@@ -0,0 +1,9 @@
+SELECT
+    city,
+    first_name,
+    last_name
+FROM
+    sales.customers
+ORDER BY
+    city,
+    first_name;
