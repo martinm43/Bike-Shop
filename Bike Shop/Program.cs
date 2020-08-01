04727ce538857303668a6321131ce73579391129
MA Miller <miller.m.43@gmail.com>
2020-08-01 17:45:45 -0400

    working C# SQL execution script

--- Bike Shop/Program.cs
+++ Bike Shop/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.IO;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
 
 namespace Bike_Shop
 {
@@ -8,16 +11,20 @@ namespace Bike_Shop
         static void Main(string[] args)
         {
             // Integrated Security=SSPI is required for Windows Authentication login.
-            var cs = @"Server=localhost\SQLEXPRESS;Database=bikeshop;Trusted_Connection=True;Integrated Security=SSPI;";
-            var stm = "SELECT @@VERSION";
+            string cs = @"Server=localhost\SQLEXPRESS;Database=bikeshop;Trusted_Connection=True;Integrated Security=SSPI;";
+            string script = File.ReadAllText(@"C:\Users\Owner.Owner-PC\source\repos\Bike Shop\customer_list.sql");
+            SqlConnection conn = new SqlConnection(cs);
+            conn.Open();
+            SqlCommand command = new SqlCommand(script, conn);
 
-            using var con = new SqlConnection(cs);
-            con.Open();
-
-            using var cmd = new SqlCommand(stm, con);
-            string version = cmd.ExecuteScalar().ToString();
-
-            Console.WriteLine(version);
+            SqlDataReader dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                Console.WriteLine(dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2));
+            }
+            dataReader.Close();
+            command.Dispose();
+            conn.Close();
         }
     }
 }
